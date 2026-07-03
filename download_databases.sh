4f97c4dede700cda91aa5c1f7e77b5f7aeb4d4be
HaydeSaracho <haydesaracho@gmail.com>
2026-07-02 22:08:15 -0300

    Update download_databases.sh: add PlasAnn databases, fix order and counter

--- download_databases.sh
+++ download_databases.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 set -e
-
 echo "================================================"
 echo "  PlasAnnotatoR - Database Download"
 echo "================================================"
-
-mkdir -p data/plsdb/meta data/indexes data/models
+mkdir -p data/plsdb/meta data/indexes data/models data/plasann
 
 # RF model
 echo ""
-echo "[1/6] Downloading RF model..."
-wget -O data/models/rf_model.zip "https://zenodo.org/records/20348780/files/rf_model.zip"
+echo "[1/7] Downloading RF model..."
+wget -O data/models/rf_model.zip "https://zenodo.org/records/20348779/files/rf_model.zip"
 unzip data/models/rf_model.zip -d data/models/
 rm data/models/rf_model.zip
 echo "Done."
 
 # PLSDB 2025 sequences
 echo ""
-echo "[2/6] Downloading PLSDB 2025 sequences (~7 GB)..."
+echo "[2/7] Downloading PLSDB 2025 sequences (~7 GB)..."
 wget -O data/plsdb/sequences.fasta "https://ccb-microbe.cs.uni-saarland.de/plsdb2025/download_fasta"
 echo "Done."
 
 # PLSDB 2025 metadata
 echo ""
-echo "[3/6] Downloading PLSDB 2025 metadata (~3.4 GB)..."
+echo "[3/7] Downloading PLSDB 2025 metadata (~3.4 GB)..."
 wget -O data/plsdb/meta/meta.tar.gz "https://ccb-microbe.cs.uni-saarland.de/plsdb2025/download_meta.tar.gz"
 tar -xzf data/plsdb/meta/meta.tar.gz -C data/plsdb/meta/
 rm data/plsdb/meta/meta.tar.gz
@@ -31,7 +29,7 @@ echo "Done."
 
 # CARD
 echo ""
-echo "[4/6] Downloading CARD..."
+echo "[4/7] Downloading CARD..."
 wget -O data/indexes/card.tar.bz2 "https://card.mcmaster.ca/latest/data"
 tar -xjf data/indexes/card.tar.bz2 -C data/indexes/
 rm data/indexes/card.tar.bz2
@@ -39,7 +37,7 @@ echo "Done."
 
 # MIBiG 4.0
 echo ""
-echo "[5/6] Downloading MIBiG 4.0 nucleotide sequences..."
+echo "[5/7] Downloading MIBiG 4.0 nucleotide sequences..."
 wget -O data/indexes/mibig_4.0.zip "https://zenodo.org/records/20350302/files/mibig_4.0.zip"
 unzip data/indexes/mibig_4.0.zip -d data/indexes/
 rm data/indexes/mibig_4.0.zip
@@ -47,7 +45,7 @@ echo "Done."
 
 # CAZy
 echo ""
-echo "[6/6] Downloading CAZy database (CAZyDB.07242025, 2 GB)..."
+echo "[6/7] Downloading CAZy database (CAZyDB.07242025, 1.2 GB)..."
 echo "Note: CAZy is updated regularly. This version corresponds to July 2025."
 echo "To use a more recent version, download manually from:"
 echo "https://pro.unl.edu/dbCAN2/browse_download.php"
@@ -55,6 +53,14 @@ echo "and rename the file to cazy.fasta in data/indexes/"
 wget -O data/indexes/cazy.zip "https://zenodo.org/records/20350742/files/cazy.zip"
 unzip data/indexes/cazy.zip -d data/indexes/
 rm data/indexes/cazy.zip
+echo "Done."
+
+# PlasAnn functional databases
+echo ""
+echo "[7/7] Downloading PlasAnn functional databases..."
+wget -O data/plasann/plasann_databases.zip "https://zenodo.org/records/20501577/files/plasann_databases.zip"
+unzip data/plasann/plasann_databases.zip -d data/plasann/
+rm data/plasann/plasann_databases.zip
 echo "Done."
 
 # PLASMe database
@@ -72,11 +78,3 @@ echo "  You can now run the pipeline:"
 echo "  micromamba activate plasannotator"
 echo "  python main.py -i input.fasta -o results/"
 echo "================================================"
-# PlasAnn functional databases
-echo ""
-echo "Downloading PlasAnn functional databases..."
-wget -O data/plasann/plasann_databases.zip "https://zenodo.org/records/20501577/files/plasann_databases.zip"
-mkdir -p data/plasann
-unzip data/plasann/plasann_databases.zip -d data/plasann/
-rm data/plasann/plasann_databases.zip
-echo "Done."
